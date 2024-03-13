19e59c73e252d8b0191c3d48d891dd0d8c792cba
fatih <fsenyigit8@gmail.com>
2024-03-13 16:00:39 +0300

    selectors

---
+++ app.js
@@ -0,0 +1,10 @@
+// selectors
+
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.seat:not(.reserved)')
+const ticketAmount = document.querySelector('#count')
+const total = document.querySelector('#total')
+const movieInfo = document.querySelector('.movieInfo')
+const form = document.querySelector('.form')
+const movieInput = document.querySelector('#input')
+const message = document.querySelector('.message')
